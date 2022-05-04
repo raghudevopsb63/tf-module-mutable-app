b94c95f12970c2516706b70eff3fd734b7de5aeb
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-04 07:15:10 +0530

    Update terraform/tf-module-mutable-app/

---
+++ tg.tf
@@ -0,0 +1,14 @@
+resource "aws_lb_target_group" "app" {
+  name     = "${var.COMPONENT}-${var.ENV}"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
+}
+
+resource "aws_lb_target_group_attachment" "instance-attach" {
+  count            = var.SPOT_INSTANCE_COUNT + var.OD_INSTANCE_COUNT
+  target_group_arn = aws_lb_target_group.app.arn
+  target_id        = element(local.ALL_INSTANCE_IDS, count.index)
+  port             = 8080
+}
+
