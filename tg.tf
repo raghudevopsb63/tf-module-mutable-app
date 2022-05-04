8f35b16d5ca58b95efbb8ec78ed3e953ddad0708
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-04 07:29:30 +0530

    Update terraform/tf-module-mutable-app/

--- tg.tf
+++ tg.tf
@@ -12,3 +12,19 @@ resource "aws_lb_target_group_attachment" "instance-attach" {
   port             = 8080
 }
 
+resource "aws_lb_listener_rule" "app_rule" {
+  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ZONE}"]
+    }
+  }
+}
+
