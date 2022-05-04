8c610450bc20a1e6613cbc0d22986470c487e176
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-04 07:04:43 +0530

    Update terraform/tf-module-mutable-app/

--- app-deploy.tf
+++ app-deploy.tf
@@ -1,4 +1,7 @@
 resource "null_resource" "app-deploy" {
+  triggers = {
+    abc = var.APP_VERSION
+  }
   count = var.SPOT_INSTANCE_COUNT + var.OD_INSTANCE_COUNT
   provisioner "remote-exec" {
     connection {
