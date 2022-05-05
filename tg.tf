985a01b0d386c7cb9e9fbe3884411fe613f1c853
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-05 06:51:10 +0530

    Update terraform/tf-module-mutable-app/

--- tg.tf
+++ tg.tf
@@ -32,3 +32,16 @@ resource "random_integer" "lb-rule_priority" {
   min = 100
   max = 500
 }
+
+resource "aws_lb_listener" "public_lb_listener" {
+  count             = var.LB_TYPE == "public" ? 1 : 0
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_LISTENER_ARN
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
+
