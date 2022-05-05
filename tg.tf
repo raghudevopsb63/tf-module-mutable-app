d0f95b764867695e64ba014b722922696921ec4e
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-05 07:10:45 +0530

    Update terraform/tf-module-mutable-app/

--- tg.tf
+++ tg.tf
@@ -1,8 +1,17 @@
 resource "aws_lb_target_group" "app" {
   name     = "${var.COMPONENT}-${var.ENV}"
-  port     = 8080
+  port     = var.APP_PORT
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    internal            = 5
+    path                = "/health"
+    timeout             = 5
+    unhealthy_threshold = 2
+
+  }
 }
 
 resource "aws_lb_target_group_attachment" "instance-attach" {
