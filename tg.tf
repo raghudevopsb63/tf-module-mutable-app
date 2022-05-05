5c98da2d3b244ffee24167d840fe596f04ad17fb
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-05 06:31:09 +0530

    Update terraform/tf-module-mutable-app/

--- tg.tf
+++ tg.tf
@@ -14,7 +14,7 @@ resource "aws_lb_target_group_attachment" "instance-attach" {
 
 resource "aws_lb_listener_rule" "app_rule" {
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
-  priority     = 100
+  priority     = random_integer.lb-rule_priority.result
 
   action {
     type             = "forward"
@@ -28,3 +28,7 @@ resource "aws_lb_listener_rule" "app_rule" {
   }
 }
 
+resource "random_integer" "lb-rule_priority" {
+  min = 100
+  max = 500
+}
