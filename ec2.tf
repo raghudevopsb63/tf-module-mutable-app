a050cd6ee4d812d2f549347a0925ed6198639f5c
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-09 06:28:43 +0530

    Update terraform/tf-module-mutable-app/

--- ec2.tf
+++ ec2.tf
@@ -5,6 +5,7 @@ resource "aws_spot_instance_request" "spot" {
   wait_for_fulfillment   = true
   vpc_security_group_ids = [aws_security_group.allow_app.id]
   subnet_id              = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
+  iam_instance_profile   = ""
 
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}"
